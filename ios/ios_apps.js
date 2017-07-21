963e41178908d2cc546fb28204121100ec15dacd
aiguo_flt <aiguo.yin@fleetcor.com>
2017-07-21 00:15:33 -0700

    web site upgrade.

--- ios/ios_apps.js
+++ ios/ios_apps.js
@@ -1,4 +1,6 @@
-var ios_apps=[
-"Web Reader", "ios/webreader/", "01/13/2016", "1.2.0.e0b99f95",
-"", "", "", ""
-];
+[
+  {"name":"Zebra Alphabet Nothing Happend", "path":"/ios/alphabet/", "date":"02/13/2016", "version":"1.0.0.170522"},
+  {"name":"Web Reader", "path":"/ios/webreader/", "date":"01/13/2016", "version":"1.0.0.170522"},
+  {"name":"Zebra Alphabet", "path":"/ios/alphabet1/", "date":"02/13/2016", "version":"1.0.0.170522"},
+  {"name":"Web Reader", "path":"/ios/webreader1/", "date":"01/13/2016", "version":"1.0.0.170522"},
+]
