c67c7711894ecac4a8106a763ccb38f43d6b106c
aiguo_flt <aiguo.yin@fleetcor.com>
2017-07-21 16:51:00 -0700

    remove test code

--- ios/ios_apps.js
+++ ios/ios_apps.js
@@ -1,6 +1,3 @@
 [
-  {"name":"Zebra Alphabet Nothing Happend", "path":"/ios/alphabet/", "date":"02/13/2016", "version":"1.0.0.170522"},
   {"name":"Web Reader", "path":"/ios/webreader/", "date":"01/13/2016", "version":"1.0.0.170522"},
-  {"name":"Zebra Alphabet", "path":"/ios/alphabet1/", "date":"02/13/2016", "version":"1.0.0.170522"},
-  {"name":"Web Reader", "path":"/ios/webreader1/", "date":"01/13/2016", "version":"1.0.0.170522"},
 ]
