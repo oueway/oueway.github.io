f4338f1f8c869dd3e7ed73f23d820e1ea1e4be52
aiguo_flt <aiguo.yin@fleetcor.com>
2017-07-21 17:21:09 -0700

    remove duplicated app

--- ios/ios_apps.js
+++ ios/ios_apps.js
@@ -1,4 +1,3 @@
 [
 {"name":"Web Reader", "path":"ios/webreader/", "date":"07/21/2017", "version":"1.0.0.1707210"}, 
-  {"name":"Web Reader", "path":"/ios/webreader/", "date":"01/13/2016", "version":"1.0.0.170522"},
 ]
