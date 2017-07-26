735a0ac85c09a74b2ae943dd47d2e3c9ada599cd
aiguo_flt <aiguo.yin@fleetcor.com>
2017-07-25 23:37:49 -0700

    apps information changes

--- ios/alphabet/more_dev.js
+++
@@ -1,2 +0,0 @@
-var more_builds=[
-];
---
+++ ios/apps.js
@@ -0,0 +1,4 @@
+[
+{"name":"Web Reader", "path":"ios/webreader/", "date":"07/21/2017", "version":"1.0.0.1707210"},
+{"name":"Alphabet", "path":"ios/alphabet/", "date":"07/21/2017", "version":"1.0.0.1707210"}
+]
