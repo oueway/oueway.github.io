21f2d4f8f12d6e02b4101b77d4dd7c3d18d49b4d
oueway <oueway.forest@gmail.com>
2025-11-29 15:08:35 -0500

    update website

---
+++ ios/apps.js
@@ -0,0 +1,6 @@
+[
+{"name":"Zebra Alphabet", "path":"ios/alphabet/", "date":"08/23/2017", "version":"1.1.0.1708230"}, 
+{"name":"Zebra Alphabet", "path":"ios/alphabet/", "date":"08/23/2017", "version":"1.1.0.1708230"}, 
+{"name":"Web Reader", "path":"ios/webreader/", "date":"07/21/2017", "version":"1.0.0.1707210"},
+{"name":"Alphabet", "path":"ios/alphabet/", "date":"07/21/2017", "version":"1.0.0.1707210"}
+]
